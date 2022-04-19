90813890c16436cdda69b81aa5b338353ef0000f
ginohmk <michotall95@gmail.com>
2022-04-19 11:04:41 +0100

    Add TDD to return attr in class laabel

--- application/label.rb
+++ application/label.rb
@@ -1,3 +1,4 @@
 # Class Label
 class Label
+  
 end
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,12 +1,22 @@
 require_relative '../application/label'
 describe 'Class label' do
   before(:all) do
-    @label = Label.new
+    @label = Label.new('movie', 'green')
   end
 
   context 'Label class instance' do
     it '@label Should be an instance of class Label' do
       expect(@label).to be_an_instance_of Label
+    end
+  end
+
+  context '@label should have attribute color and title' do
+    it '@label.title Should return title' do
+      expect(@label).to eq 'movie'
+    end
+
+    it '@label.color Should return color' do
+      expect(@label).to eq 'green'
     end
   end
 end

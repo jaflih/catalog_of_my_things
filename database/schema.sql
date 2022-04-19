6479bb9ecebd1d27794134c818e61bc21873bbbe
debas-31 <debas31@gmail.com>
2022-04-19 15:26:57 +0300

    Create genres table

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE music_albums (
   on_spotify BOOLEAN,
   FOREIGN KEY(id) REFERENCES item(id)
 );
+
+CREATE TABLE genres (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100)
+  PRIMARY KEY(id)
+);

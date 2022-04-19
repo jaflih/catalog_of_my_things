84eabdff469fee28392a6413439997486f000e12
jihaneh <jijih@hotmail.fr>
2022-04-19 10:54:21 +0200

    Fix linters

--- application/author.rb
+++ application/author.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # Author model
 class Author
   attr_accessor :first_name, :last_name
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require './application/author'
 # require './application/item'
 # require './application/genre'

bcbd9165d3233120e9b6a5ec354bcfc38eb0bda6
debas-31 <debas31@gmail.com>
2022-04-19 12:22:15 +0300

    Create MusicAlbum class and setup constructor

---
+++ application/music_album.rb
@@ -0,0 +1,13 @@
+require_relative './item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :publish_date
+  attr-reader :items
+
+  def initialize(on_spotify, publish_date)
+    super(publish_date, archived)
+    @id = Random.rand(1..1000)
+    @on_spotify = on_spotify
+    @items = []
+  end 
+end

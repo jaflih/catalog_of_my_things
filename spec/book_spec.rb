cbc58a74697af09eca5361d6d220462cc09ef5f1
ginohmk <michotall95@gmail.com>
2022-04-19 08:57:17 +0100

    Add TDD for Book instance

---
+++ spec/book_spec.rb
@@ -0,0 +1,10 @@
+describe 'Book class' do
+  before(:all) do
+    @book = Book.new
+  end
+  context 'Create the book class' do
+    it 'Book class should have an instance Book class' do
+      expect(@book).to be_an_instance_of Book
+    end
+  end
+end

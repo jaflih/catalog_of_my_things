ffaa4ea17ff1c5ed15b78f4a3fa853967a2cb999
ginohmk <michotall95@gmail.com>
2022-04-19 10:30:26 +0100

    Add TDD for method can-be_archived

--- application/book.rb
+++ application/book.rb
@@ -1,8 +1,10 @@
+require_relative './item'
 # Class Book
-class Book
+class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state)
+  def initialize(publisher, cover_state, publish_date, archived)
+    super(publish_date, archived)
     @publisher = publisher
     @cover_state = cover_state
   end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../application/book'
 describe 'Book class' do
   before(:all) do
-    @book = Book.new('james bond', 'cover_state')
+    @book = Book.new('james bond', 'cover_state', '2021-10-02', false)
   end
   context 'Create the book class' do
     it 'book class should have an instance Book class' do
@@ -13,11 +13,16 @@ describe 'Book class' do
     it 'Publisher attribute should return james bond' do
       expect(@book.publisher).to eq 'james bond'
     end
-  end
 
-  context 'book to have attribute publisher and cover state' do
     it 'cover_state attribute should return cover_state' do
       expect(@book.cover_state).to eq 'cover_state'
+    end
+  end
+
+  context 'can_archived method' do
+    it 'can_archived should return true if parent method return true or cover_state equals bad' do
+      @book = Book.new('james bond', 'bad', '2021-10-02', false)
+      expect(@book.can_be_archived?).to eq true
     end
   end
 end

d616b23d017875d7eb985e1644c15462e5a8103a
jihaneh <jijih@hotmail.fr>
2022-04-19 11:28:53 +0200

    Add Game class

---
+++ application/game.rb
@@ -0,0 +1,11 @@
+require './application/item'
+
+class Game < Item
+  attr_accessor :multiplayer, :last_played_at
+
+  def initialize(publish_date, archived, multiplayer, last_played_at)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+end

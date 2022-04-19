a34002c839f3f91e12faa8073e8571ccd3734886
ginohmk <michotall95@gmail.com>
2022-04-19 10:11:18 +0100

    Add TDD for publisher attr in class Book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,12 +1,17 @@
 require_relative '../application/book'
 describe 'Book class' do
   before(:all) do
-    @book = Book.new
+    @book = Book.new('james bond', 'cover_state')
   end
   context 'Create the book class' do
-    it 'Book class should have an instance Book class' do
+    it 'book class should have an instance Book class' do
       expect(@book).to be_an_instance_of Book
     end
   end
-end
 
+  context 'book to have attribute publisher and cover state' do
+    it 'Publisher attribute should return james bond' do
+      expect(book.publisher).to eq 'james bond'
+    end
+  end
+end

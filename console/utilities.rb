46817e571d321b7357877c58dd4f4b0294213fd0
debas-31 <debas31@gmail.com>
2022-04-20 16:12:40 +0300

    Add my-task option in the Util class

--- console/utilities.rb
+++ console/utilities.rb
@@ -3,6 +3,9 @@ class Util
     puts " Please choose an option by entering a number:
     1 -List all books
     2 -Create a book
+    4 -List all music albums
+    5 -List all genres
+    6 -Create a music album
     0 -Exit"
   end
 end

cd7cd4fe67591838363f5c99d43f4ef061a5a6fd
jihaneh <jijih@hotmail.fr>
2022-04-20 12:06:43 +0200

    Update menu option

--- console/utilities.rb
+++ console/utilities.rb
@@ -3,6 +3,9 @@ class Util
     puts " Please choose an option by entering a number:
     1 -List all books
     2 -Create a book
+    7 -List all games
+    8 -Create a game
+    9 -List all authors
     0 -Exit"
   end
 end

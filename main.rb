6844853e20a7316ad70196db2b340d61479270ca
jihaneh <jijih@hotmail.fr>
2022-04-20 11:57:32 +0200

    Add options

--- main.rb
+++ main.rb
@@ -1,18 +1,25 @@
 require './console/app'
-require './utilities'
+require './console/utilities'
 
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/MethodLength
 def main()
   app = App.new
   util = Util.new
   option = nil
   while option != '0'
     util.menu_display
-    option = gets.chomp
+    option = $stdin.gets.chomp
     case option
     when '1'
       app.list_books
     when '2'
       app.create_book
+    when '7'
+      app.list_games
+    when '8'
+      app.create_game
+    when '9'
+      app.list_authors
     when '0'
       puts "\n"
       puts 'Exited successfully, thank you for using this app 👍😊💪'
@@ -21,5 +28,6 @@ def main()
     end
   end
 end
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength
 
 main

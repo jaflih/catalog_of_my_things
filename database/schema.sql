ae09575dca0871594b41d77f67d7a1462e92bdd8
debas-31 <debas31@gmail.com>
2022-04-20 11:30:10 +0300

    Modify schema.sql file

--- database/schema.sql
+++ database/schema.sql
@@ -1,10 +1,7 @@
 CREATE DATABASE catalog;
 CREATE TABLE music_albums (
-  id INT,
-  /* name VARCHAR(100),*/
   on_spotify BOOLEAN,
-  FOREIGN KEY(id) REFERENCES item(id)
-);
+)INHERITS(items);
 
 CREATE TABLE genres (
   id INT GENERATED ALWAYS AS IDENTITY,

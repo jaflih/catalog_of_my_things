6fa220f969cecd992c4d913e089903f2c2dc09a3
debas-31 <debas31@gmail.com>
2022-04-21 07:38:12 +0300

    Add option to the main.rb file

--- main.rb
+++ main.rb
@@ -13,6 +13,12 @@ def main()
       app.list_books
     when '2'
       app.create_book
+    when '4'
+      app.list_music_albums
+    when '5'
+      app.list_genres
+    when '6'
+      app.create_music_album
     when '0'
       puts "\n"
       puts 'Exited successfully, thank you for using this app 👍😊💪'

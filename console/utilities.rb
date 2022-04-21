8a66bf069fe896594f0e23526d6da3f377048108
jihaneh <jijih@hotmail.fr>
2022-04-21 15:34:50 +0200

    Update the menu

--- console/utilities.rb
+++ console/utilities.rb
@@ -3,14 +3,14 @@ class Util
     puts
     puts " Please choose an option by entering a number:
     1 -List all books
-    2 -Create a book
-    3 -List all labels
-    4 -List all music albums
-    5 -List all genres
-    6 -Add a music album
+    2 -List all labels
+    3 -List all music albums
+    4 -List all genres
     7 -List all games
-    8 -Create a game
     9 -List all authors
+    2 -Create a book
+    6 -Create a music album
+    8 -Create a game
     0 -Exit"
   end
 end

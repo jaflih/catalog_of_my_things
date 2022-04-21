5407e7f2a8f22743998008acf69442c179f16533
debas-31 <debas31@gmail.com>
2022-04-21 12:05:19 +0300

    Merge development with thisbranch

--- main.rb
+++ main.rb
@@ -14,21 +14,18 @@ def main()
       app.list_books
     when '2'
       app.create_book
-<<<<<<< HEAD
     when '4'
       app.list_music_albums
     when '5'
       app.list_genres
     when '6'
       app.add_music_album
-=======
     when '7'
       app.list_games
     when '8'
       app.create_game
     when '9'
       app.list_authors
->>>>>>> development
     when '0'
       puts "\n"
       puts 'Exited successfully, thank you for using this app 👍😊💪'
